1a5f6465f580f4ace370228ac56532a504307b9a
harikrishna <rajanenihari@gmail.com>
2018-04-03 16:36:15 +0530

    done

--- main.js
+++ main.js
@@ -9,7 +9,28 @@ function loadJSON(file,callback){
 };
 xhr.send();
 }
-loadJSON("data.json",function(text){
+loadJSON("data.json",function(text) {
   let data=JSON.parse(text);
-  console.log(data);
+  console.log(data.career.info);
+  career(data.career);
+  eduction(data.eduction);
 })
+var right= document.querySelector(".content");
+function career(car){
+  console.log(car.info);
+  var h3=document.createElement("h3");
+    h3.textContent="career objective";
+    right.appendChild(h3);
+    var hr=document.createElement("hr");
+    right.appendChild(hr);
+    var p=document.createElement("p");
+    p.textContent=car.info;
+    right.appendChild(p);
+  }
+  function eduction(edu){
+      var h3=document.createElement("h3");
+      h3.textContent="EDUCATION";
+      right.appendChild(h3);
+      var hr=document.createElement("hr");
+        right.appendChild(hr);
+      }
